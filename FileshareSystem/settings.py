6604434ddf16cab1de35292dfcad9b88e06d1257
AndyLiang97 <junjl97@vt.edu>
2019-07-24 20:08:56 -0400

    change STATIC_ROOT to s3 absolute path

--- FileshareSystem/settings.py
+++ FileshareSystem/settings.py
@@ -163,7 +163,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'users/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'FileshareSystem.storage_backends.MediaStorage'
